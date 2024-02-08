9daf80742a2ef4dd5ce165d293664dbfe827d354
Naoya Furudono <naoyafurudono@gmail.com>
2024-02-09 03:05:55 +0900

    link

--- too-complicated-to-tweet/app/page.tsx
+++ too-complicated-to-tweet/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Landing() {
@@ -6,6 +7,14 @@ export default function Landing() {
     <main className={styles.main}>
       こんにちは！
       <Image src="/thumbnail.png" width={100} height={100} alt="ちんあなごロゴでござる"></Image>
+      <div>
+        <h2>最新の記事</h2>
+        <ol>
+          <li>
+            <Link href="/hi">初めての記事です</Link>
+          </li>
+        </ol>
+      </div>
     </main>
   )
 }

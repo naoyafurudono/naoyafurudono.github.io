7dfad65f8310dddf4e05e14e77ffd6becc2ffa1c
Naoya Furudono <naoyafurudono@gmail.com>
2024-02-09 23:24:42 +0900

    update

--- too-complicated-to-tweet/backend/main.go
+++ too-complicated-to-tweet/backend/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
-	println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }

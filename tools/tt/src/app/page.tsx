4fe7d7fa40a22d7862c6d76bda3ccd017f5230ef
Naoya Furudono <naoyafurudono@gmail.com>
2024-08-14 02:38:50 +0900

    雑な概要を出す

--- tools/tt/src/app/page.tsx
+++ tools/tt/src/app/page.tsx
@@ -1,5 +1,6 @@
+import { type Article, listArticles } from "@/lib/gateway";
+import { render } from "@/lib/render";
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
 
 type Props = {
 	params: {
@@ -10,19 +11,27 @@ const Home: NextPage<Props> = async () => {
 	return (
 		<>
 			<h1>ツイートするには長すぎる</h1>
-			<div />
+			<div>
+				{listArticles().map((article) => (
+					<ArticleSummary article={article} key={article.id} />
+				))}
+			</div>
 		</>
 	);
 };
 export default Home;
 
-// type SummaryProps = {
-// 	article: Article;
-// };
-// const ArticleSummary = ({ article }: SummaryProps) => {
-// 	return (
-// 		<article>
-// 			<h1>{article.title}</h1>
-// 		</article>
-// 	);
-// };
+type SummaryProps = {
+	article: Article;
+};
+const ArticleSummary = async ({ article }: SummaryProps) => {
+	const res = await render(article);
+	return (
+		<article>
+			<h1>
+				<a href={`/posts/${article.id}`}>{res.title}</a>
+			</h1>
+  	  <time>{res.date}</time>
+		</article>
+	);
+};

f1951bd0a59e09a027b9557453932c2b61a78f51
Naoya Furudono <naoyafurudono@gmail.com>
2024-10-14 13:27:27 +0900

    update

--- tools/tt/src/app/layout.tsx
+++ tools/tt/src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-	title: "ブログ",
-	description: "ブログです",
+	title: "日記",
+	description: "これは日記です",
 	authors: [
 		{
 			name: "Naoya Furudono",

d481ab3992e2e2b6d107ab55f3cdfee889abb914
Naoya Furudono <naoyafurudono@gmail.com>
2024-12-30 12:08:08 +0900

    fix typo

--- tools/tt/src/lib/util.test.ts
+++ tools/tt/src/lib/util.test.ts
@@ -1,27 +1,27 @@
 import { describe, expect, it } from "vitest";
 import { lexOrder } from "./util";
 
-type Ord = "eq" | "less" | "greate";
-function toOrdr(n: number): Ord {
+type Ord = "eq" | "less" | "greater";
+function toOrd(n: number): Ord {
 	if (n === 0) {
 		return "eq";
 	}
 	if (n < 0) {
 		return "less";
 	}
-	return "greate";
+	return "greater";
 }
 describe("lexOrder", () => {
 	it("a < b", () => {
-		expect(toOrdr(lexOrder("a", "b")) === "less").toBeTruthy();
+		expect(toOrd(lexOrder("a", "b")) === "less").toBeTruthy();
 	});
 	it("a = a", () => {
-		expect(toOrdr(lexOrder("a", "a")) === "eq").toBeTruthy();
+		expect(toOrd(lexOrder("a", "a")) === "eq").toBeTruthy();
 	});
 	it("あ < い", () => {
-		expect(toOrdr(lexOrder("あ", "い")) === "less").toBeTruthy();
+		expect(toOrd(lexOrder("あ", "い")) === "less").toBeTruthy();
 	});
 	it("たいし　> たいこ", () => {
-		expect(toOrdr(lexOrder("たいし", "たいこ")) === "greate").toBeTruthy();
+		expect(toOrd(lexOrder("たいし", "たいこ")) === "greater").toBeTruthy();
 	});
 });
